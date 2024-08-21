901a195c6d19aac122d023c1a70274e85bbafded
sebastian <sebastian.guhl@student.uni-luebeck.de>
2024-08-21 04:09:08 +0200

    Add Correctness check for answers

--- script.js
+++ script.js
@@ -4,27 +4,46 @@ const questions = [
         answers: [
             {
                 answer: "Test Answer 1.1",
-                selected: false
+                correct: true
             }, 
             {
                 answer: "Test Answer 1.2",
-                selected: false
+                correct: false
             },
             {
                 answer: "Test Answer 1.3",
-                selected: false
+                correct: false
             },
             {
                 answer: "Test Answer 1.4",
-                selected: false
+                correct: false
             }
-        ]
+        ],
+        confirmed: false,
+        correctAnswer: false
     },
     {
         question: "Test Question 2",
         answers: [
-            "Test Answer 2.1", "Test Answer 2.2", "Test Answer 2.3", "Test Answer 2.4"
-        ]
+            {
+                answer: "Test Answer 2.1",
+                correct: true
+            }, 
+            {
+                answer: "Test Answer 2.2",
+                correct: false
+            },
+            {
+                answer: "Test Answer 2.3",
+                correct: false
+            },
+            {
+                answer: "Test Answer 2.4",
+                correct: false
+            }
+        ],
+        confirmed: false,
+        correctAnswer: false
     }
 ]
 
@@ -33,6 +52,8 @@ const answerButtons = document.getElementById("answers")
 const confirmButton = document.getElementById("confirm")
 const nextButton = document.getElementById("next")
 const previousButton = document.getElementById("previous")
+const score = document.getElementById("score")
+const questionResult = document.getElementById("question_result")
 
 let questionCounter = 0;
 let correctAnswers = 0;
@@ -43,20 +64,20 @@ function startQuiz() {
     confirmButton.innerHTML = "Bestätigen";
     nextButton.addEventListener("click", nextButtonClicked)
     previousButton.addEventListener("click", previousButtonClicked)
+    confirmButton.addEventListener("click", confirmButtonClicked)
     showQuestion()
 }
 
 function nextButtonClicked() {
     if(questionCounter < questions.length-1) {
-        questionCounter++;
+        questionCounter++
     }
-    console.log(questionCounter)
     showQuestion()
 }
 
 function previousButtonClicked() {
     if(questionCounter > 0) {
-        questionCounter--;
+        questionCounter--
     }
     showQuestion()
 }
@@ -66,23 +87,52 @@ function showQuestion() {
         answerButtons.removeChild(answerButtons.firstChild)
     }
     let currentQuestion = questions[questionCounter]
-    questionTitle.innerHTML = "Frage Nr. " + questionCounter + ": " + currentQuestion.question
+    questionTitle.innerHTML = "Frage Nr. " + (questionCounter+1) + ": " + currentQuestion.question
 
     currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.answer;
-        button.classList.add("button");
-        answerButtons.appendChild(button);
-        button.dataset.selected = answer.selected
-        if(answer.selected) {
-            button.classList.add("selected")
-        }
-        if(answer.selected) {
-            button.addEventListener("click", unselectAnswer)
+        const button = document.createElement("button")
+        button.innerHTML = answer.answer
+        button.classList.add("button")
+        answerButtons.appendChild(button)
+        button.dataset.selected = false
+        if(currentQuestion.confirmed) {
+            if(answer.correct) {
+                button.classList.add("correct")
+            } else {
+                button.classList.add("wrong")
+            }
         } else {
             button.addEventListener("click", selectAnswer)
         }
     })
+    score.innerHTML = "Ergebnis: " + correctAnswers
+    if(currentQuestion.confirmed) {
+        questionResult.innerHTML = currentQuestion.correctAnswer ? "Korrekt!" : "Leider Falsch!"
+        confirmButton.disabled = true
+    } else {
+        questionResult.innerHTML = ""
+        confirmButton.disabled = false
+    }
+}
+
+function confirmButtonClicked() {
+    let currentQuestion = questions[questionCounter]
+    let correctAnswer = true
+    let answerCounter = 0
+    answerButtons.childNodes.forEach(answerButton => {
+        let currentAnswer = currentQuestion.answers[answerCounter]
+        const selected = answerButton.dataset.selected === "true"
+        if (selected !== currentAnswer.correct) {
+            correctAnswer = false
+        }
+        answerCounter++
+    })
+    if(correctAnswer) {
+        correctAnswers++
+    }
+    currentQuestion.confirmed = true
+    currentQuestion.correctAnswer = correctAnswer
+    showQuestion()
 }
 
 function selectAnswer(e) {
